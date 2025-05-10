180e49472e070654f92dee4cc812c20d5c6b64f1
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-10 11:28:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+// const makeChange = (c) => {
+//   // your name here
+// };
+
+// // Do not the change the code below
+// const c = prompt("Enter c: ");
+// // alert(JSON.stringify(makeChange(c)));
+
+
+
 const makeChange = (c) => {
-  // your name here
+  c = parseInt(c); // ensure input is an integer
+
+  const result = {
+    q: Math.floor(c / 25),
+    d: Math.floor((c % 25) / 10),
+    n: Math.floor((c % 25 % 10) / 5),
+    p: c % 5
+  };
+
+  return result;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
